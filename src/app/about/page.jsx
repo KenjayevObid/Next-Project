c81f161e24265cc45f552f71a9640b73fb4fa7ea
KenjayevObid <114725656+KenjayevObid@users.noreply.github.com>
2024-01-13 15:21:25 +0500

    first commit

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import AboutPage from '@/components/About/about';
+
+
 const About= () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div >
         <AboutPage />
     </div>
   )
--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { Navbar } from "flowbite-react";
+import { Button, Navbar } from "flowbite-react";
 import { BsDribbble } from "react-icons/bs";
 import { usePathname } from "next/navigation";
 import { DarkThemeToggle, Flowbite } from "flowbite-react";
@@ -9,13 +9,13 @@ import { DarkThemeToggle, Flowbite } from "flowbite-react";
 export default function NavbarPage() {
   const pathname = usePathname();
   const link = [
-   
-    { id: 1, href: "/home", title: "Home", active: false },
-    { id: 2, href: "/about", title: "Abaut", active: false },
-    { id: 3, href: "/contact", title: "Contact", active: false },
-    { id: 4, href: "/blog", title: "Blog", active: false },   
-    { id: 5, href: "/login", title: "Login", active: false },
+    { id: 1, href: "/home", title: "Home", active: true },
+    { id: 2, href: "/about", title: "Abaut", active: true },
+    { id: 3, href: "/contact", title: "Contact", active: true },
+    { id: 4, href: "/blog", title: "Blog", active: true },
   ];
+  const session = true;
+  const isAdmin = true;
   return (
     <Navbar fluid rounded>
       <Navbar.Brand as={Link} href="https://flowbite-react.com">
@@ -29,19 +29,30 @@ export default function NavbarPage() {
       </Flowbite>
       <Navbar.Toggle />
       <Navbar.Collapse>
-        {link.map((item) => (
-          <Navbar.Link
-            key={item.id}
-            className={`${
-              pathname === item.href
-                ? "inline-block border border-red-500 rounded w-full bg-blue-500 text-white"
-                : ""
-            }`}
-            href={item.href}
-          >
-            {item.title}
-          </Navbar.Link>
-        ))}
+        {link.map(
+          (item) =>
+            item.active && (
+              <Navbar.Link
+                key={item.id}
+                className={`${
+                  pathname === item.href
+                    ? "px-3 py-1 bg-green-400 text-black"
+                    : ""
+                }`}
+                href={item.href}
+              >
+                {item.title}
+              </Navbar.Link>
+            )
+        )}
+        {session ? (
+          <>
+            {isAdmin && <Navbar.Link href="/admin">Admin</Navbar.Link>}
+            <button className="px-3 py-1 bg-green-400">Logout</button>
+          </>
+        ) : (
+          <Navbar.Link href="/login">Login</Navbar.Link>
+        )}
       </Navbar.Collapse>
     </Navbar>
   );
