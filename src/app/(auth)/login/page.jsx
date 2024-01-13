e33c9b2fc282012a029944196c456a8505eab513
KenjayevObid <114725656+KenjayevObid@users.noreply.github.com>
2024-01-13 14:04:42 +0500

    second commit

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LoginPage from '@/components/Login/login'
+const Login = () => {
+  return (
+    <div>
+        <LoginPage />
+    </div>
+  )
+}
+
+export default Login
--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,36 +1,47 @@
+"use client";
 
-'use client';
-
-import Link from 'next/link';
-import { Navbar } from 'flowbite-react';
-import { BsDribbble} from 'react-icons/bs';
-import { usePathname } from 'next/navigation';
-import { DarkThemeToggle, Flowbite } from 'flowbite-react';
+import Link from "next/link";
+import { Navbar } from "flowbite-react";
+import { BsDribbble } from "react-icons/bs";
+import { usePathname } from "next/navigation";
+import { DarkThemeToggle, Flowbite } from "flowbite-react";
 
 export default function NavbarPage() {
-  const pathname = usePathname()
+  const pathname = usePathname();
   const link = [
+   
     { id: 1, href: "/home", title: "Home", active: false },
-    { id: 1, href: "/about", title: "Abaut", active: false },
-    { id: 1, href: "/contact", title: "Contact", active: false },
-    { id: 1, href: "/blog", title: "Blog", active: false },
+    { id: 2, href: "/about", title: "Abaut", active: false },
+    { id: 3, href: "/contact", title: "Contact", active: false },
+    { id: 4, href: "/blog", title: "Blog", active: false },   
+    { id: 5, href: "/login", title: "Login", active: false },
   ];
   return (
-    <Navbar fluid rounded >
+    <Navbar fluid rounded>
       <Navbar.Brand as={Link} href="https://flowbite-react.com">
-       <i>{BsDribbble}</i> 
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
+        <i>{BsDribbble}</i>
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Flowbite React
+        </span>
       </Navbar.Brand>
-      <Flowbite>     
-      <DarkThemeToggle />    
-    </Flowbite>
+      <Flowbite>
+        <DarkThemeToggle />
+      </Flowbite>
       <Navbar.Toggle />
       <Navbar.Collapse>
-      {link.map((item) => (
-          <Navbar.Link key={item.id} className={`${pathname === item.href ? 'inline-block border border-blue-500 rounded p-10 bg-blue-500 text-white' : ''}`} href={item.href}>
+        {link.map((item) => (
+          <Navbar.Link
+            key={item.id}
+            className={`${
+              pathname === item.href
+                ? "inline-block border border-red-500 rounded w-full bg-blue-500 text-white"
+                : ""
+            }`}
+            href={item.href}
+          >
             {item.title}
           </Navbar.Link>
-        ))}  
+        ))}
       </Navbar.Collapse>
     </Navbar>
   );
